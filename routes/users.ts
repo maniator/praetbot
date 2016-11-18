85252b7b5875f7aae92203b01631e8b871199c76
Neal Lubin <naftalilubin@gmail.com>
2016-11-17 19:23:18 -0500

    Added mongo

--- routes/users.ts
+++ routes/users.ts
@@ -1,4 +1,4 @@
-import { cookies } from '../bin/cookies';
+import { getCookies } from '../bin/cookies';
 
 var express = require('express');
 var router = express.Router();
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req : any, res : any, next : Function) {
-  res.json(cookies);
+  getCookies().then((cookies) => res.json(cookies));
 });
 
 module.exports = router;
