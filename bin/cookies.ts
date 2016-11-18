da2ff29deb15cb885c4f71ec6e8510c9a88e85f8
Neal Lubin <naftalilubin@gmail.com>
2016-11-18 13:19:34 -0500

    Fix issue where every mention for a user was being pinged

--- bin/cookies.ts
+++ bin/cookies.ts
@@ -1,53 +1,53 @@
 import {connect} from './dbConnect';
 const assert = require('assert')
 
-interface User {
+interface CookieUser {
     name: string;
     id: string;
     cookies?: number;
 }
 
-const createUser = function (user : User, collection : any, callback : Function) {
-    collection.insert(user, callback);
+const createCookieUser = function (cookieUser : CookieUser, collection : any, callback : Function) {
+    collection.insert(cookieUser, callback);
 };
 
-const updateUser = function (user : User, collection : any, callback : Function) {
-    collection.updateOne({id: user.id}, { $set: { cookies: user.cookies }}, callback);
+const updateCookieUser = function (cookieUser : CookieUser, collection : any, callback : Function) {
+    collection.updateOne({id: cookieUser.id}, { $set: { cookies: cookieUser.cookies }}, callback);
 };
 
-const updateCookie = function (user : User) : Promise<User> {
+const updateCookie = function (cookieUser : CookieUser) : Promise<CookieUser> {
     return new Promise ((res) => {
         connect(function (db : any) {
             // Get the cookie collection
             var collection = db.collection('cookie');
 
-            const userUpdated = () => {
-                res(user);
+            const cookieUserUpdated = () => {
+                res(cookieUser);
                 db.close();
             };
 
             collection.find({
-                id: user.id
-            }).toArray(function (err: any, users: User[]) {
-                if (users.length === 0) {
-                    createUser(user, collection, userUpdated);
+                id: cookieUser.id
+            }).toArray(function (err: any, CookieUsers: CookieUser[]) {
+                if (CookieUsers.length === 0) {
+                    createCookieUser(cookieUser, collection, cookieUserUpdated);
                 } else {
-                    updateUser(user, collection, userUpdated);
+                    updateCookieUser(cookieUser, collection, cookieUserUpdated);
                 }
             });
         });
     });
 };
 
-const getCookieByUserId = function (userId : string) : Promise<User> {
+const getCookieByUserId = function (cookieUserId : string) : Promise<CookieUser> {
     return new Promise((res) => {
         connect(function (db : any) {
             // Get the cookie collection
             var collection = db.collection('cookie');
 
-            collection.find({ id: userId }).toArray(function (err : any, cookies : User[]) {
+            collection.find({ id: cookieUserId }).toArray(function (err : any, cookies : CookieUser[]) {
                 if (cookies.length === 1) {
-                    res(cookies[0] as User);
+                    res(cookies[0] as CookieUser);
                 }
 
                 res(null);
@@ -58,13 +58,13 @@ const getCookieByUserId = function (userId : string) : Promise<User> {
     });
 }
 
-const getCookies = function () : Promise<User[]> {
+const getCookies = function () : Promise<CookieUser[]> {
     return new Promise((res) => {
         connect(function (db : any) {
             // Get the cookie collection
             var collection = db.collection('cookie');
 
-            collection.find({}).toArray(function (err : any, cookies : User[]) {
+            collection.find({}).toArray(function (err : any, cookies : CookieUser[]) {
                 res(cookies);
                 db.close();
             });
@@ -76,4 +76,5 @@ export {
     updateCookie,
     getCookies,
     getCookieByUserId,
+    CookieUser,
 };
