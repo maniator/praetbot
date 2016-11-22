5032dbc408fbf94c62e56b5405cec36dffc4ca7a
Neal Lubin <naftalilubin@gmail.com>
2016-11-22 11:18:34 -0500

    Moved command related commands to the command list

--- bot/command.ts
+++ bot/command.ts
@@ -5,84 +5,50 @@ import {connect} from '../bin/dbConnect';
 
 class CommandListener {
     private commandRegex: RegExp = /^\!\!([a-zA-Z]*)\s?(.*)?/;
-    private addCommandRegex: RegExp = /^\!\!addCommand ([a-zA-Z]*)\s?(.*)?/;
-    private removeCommandRegex: RegExp = /^\!\!removeCommand ([a-zA-Z]*)?/;
-    private listCommandRegex: RegExp = /^\!\!listCommands/;
 
     constructor(private botListener: any) { }
 
     respondToCommand (message: Message) {
         connect((db: any) => {
-            if (this.addCommandRegex.test(message.text)) {
-                const [, command, response] = message.text.match(this.addCommandRegex);
+            const [, command, args = ''] = message.text.match(this.commandRegex);
 
-                db.collection('commands').save({
-                    _id: command,
-                    value: response,
-                }).then((err: any, value: any) => {
-                    this.botListener.bot.postMessage(message.channel, `<@${message.user}> ${command} added!`, {as_user: true});
-                    db.close();
-                });
-            } else if (this.removeCommandRegex.test(message.text)) {
-                const [, command] = message.text.match(this.removeCommandRegex);
+            db.collection('commands').find({
+                _id: command
+            }).toArray((error: any, list: Command[] = []) => {
+                this.botListener.bot.getUserById(message.user).then((user: any) => {
+                    list.forEach((_command: Command) => {
+                        try {
+                            if (_command.value) {
+                                const value = new Function('bot', 'channel', 'user', '...args', _command.value);
+                                const response = value({
+                                    postMessage: this.botListener.bot.postMessage.bind(this.botListener.bot),
+                                    botId: this.botListener.botId,
+                                    botName: this.botListener.botName,
+                                }, message.channel, user, ...args.split(' '));
 
-                db.collection('commands').remove({
-                    _id: command,
-                }).then((err: any, value: any) => {
-                    this.botListener.bot.postMessage(message.channel, `<@${message.user}> ${command} removed!`, {as_user: true});
-                    db.close();
-                });
-            } else if (this.listCommandRegex.test(message.text)) {
-                db.collection('commands').find({}).toArray((error: any, list: Command[] = []) => {
-                    const commandList = list.map((item: any) => item._id);
-
-                    commandList.push(...commands.map((_command: Command) => _command.name));
-
-                    this.botListener.bot.postMessage(message.channel, commandList.join(', '), {as_user: true});
-
-                    db.close();
-                });
-            } else {
-                const [, command, args = ''] = message.text.match(this.commandRegex);
-
-                db.collection('commands').find({
-                    _id: command
-                }).toArray((error: any, list: Command[] = []) => {
-                    this.botListener.bot.getUserById(message.user).then((user: any) => {
-                        list.forEach((_command: Command) => {
-                            try {
-                                if (_command.value) {
-                                    const value = new Function('bot', 'channel', 'user', '...args', _command.value);
-                                    const response = value({
-                                        postMessage: this.botListener.bot.postMessage.bind(this.botListener.bot),
-                                        botId: this.botListener.botId,
-                                        botName: this.botListener.botName,
-                                    }, message.channel, user, ...args.split(' '));
-
-                                    if (response) {
-                                        this.botListener.bot.postMessage(message.channel, `${response}`, {as_user: true});
-                                    }
-                                } else {
-                                    console.log('There is no command value for this one', _command);
+                                if (response) {
+                                    this.botListener.bot.postMessage(message.channel, `${response}`, {as_user: true});
                                 }
-                            } catch (e) {
-                                this.botListener.bot.postMessage(message.channel, `<@${message.user}> there is some issue with that command. \`${e.message}\``, {as_user: true});
+                            } else {
+                                console.log('There is no command value for this one', _command);
+                            }
+                        } catch (e) {
+                            this.botListener.bot.postMessage(message.channel, `<@${message.user}> there is some issue with that command. \`${e.message}\``, {as_user: true});
+                        }
+                    });
+
+                    commands.filter((_command: Command) => _command.name === command)
+                        .forEach((_command: Command) => {
+                            if (_command.execute) {
+                                _command.execute(this.botListener.bot, message.channel, user, ...args.split(' '));
+                            } else if (_command.respond) {
+                                this.botListener.bot.postMessage(message.channel, `<@${message.user}> ${_command.respond}`, {as_user: true});
                             }
                         });
 
-                        commands.filter((_command: Command) => _command.name === command)
-                            .forEach((_command: Command) => {
-                                if (_command.execute) {
-                                    _command.execute(this.botListener.bot, message.channel, user, ...args.split(' '));
-                                } else if (_command.respond) {
-                                    this.botListener.bot.postMessage(message.channel, `<@${message.user}> ${_command.respond}`, {as_user: true});
-                                }
-                            });
-
-                        db.close();
-                    });
+                    db.close();
                 });
-            }
+            });
         });
     }
 
