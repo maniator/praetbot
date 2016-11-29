bc2c2fb1bab184d82d79dba115145e11c125c608
Neal Lubin <naftalilubin@gmail.com>
2016-11-29 15:23:54 -0500

    make server for mongo configurable

--- bin/dbConnect.ts
+++ bin/dbConnect.ts
@@ -2,10 +2,11 @@ const { MongoClient } = require('mongodb');
 
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASSWORD;
+const mongoServer = process.env.MONGO_SERVER;
 
 const connect = function (cb : Function = (() => {})) {
     // Connection URL
-    var url = `mongodb://${user}:${password}@ds157487.mlab.com:57487/praet-bot`;
+    var url = `mongodb://${user}:${password}@${mongoServer}`;
     // Use connect method to connect to the Server
     MongoClient.connect(url, function (err : Object, db : any) {
         if (!err) {
