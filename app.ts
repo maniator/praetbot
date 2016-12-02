b7a0f230cbbbecca3a05a002625fe0eefb243269
David Komer <david.komer@gmail.com>
2016-12-02 10:28:18 +0200

    dotenv setup for dev purposes

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+//loads environment config from .env file rather than require setting system config - ignored in .gitignore
+require('dotenv').config();
+
 import { Bot } from './bot/index';
 
 const apiKey : string = process.env.BOT_API_KEY;
